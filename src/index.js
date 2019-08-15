f2ae7289c71bff213ef8924d2443787c7ad38c70
Vyacheslav Serdyuk <Navarachkou@gmail.com>
2019-08-16 00:57:41 +0500

    Add more endpoints for user and task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('./db/mongoose')
+const mongoose = require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
 
@@ -18,6 +18,25 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users)
+  }).catch((err) => {
+    res.status(500).send()
+  })
+})
+
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id).then(user => {
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  }).catch(err => {
+    res.status(500).send(err)
+  })
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
 
@@ -25,6 +44,28 @@ app.post('/tasks', (req, res) => {
     res.status(201).send(task)
   }).catch((err) => {
     res.status(400).send(err)
+  })
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then(tasks => {
+    if (!tasks) {
+      return res.status(404).send()
+    }
+    res.send(tasks)
+  }).catch(err => {
+    res.status(500).send()
+  })
+})
+
+app.get('/tasks/:id', (req, res) => {
+  Task.findById(req.params.id).then(task => {
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  }).catch(err => {
+    res.status(500).send()
   })
 })
 
